852fe8ababe35f2e2052fa16dd13eda0b0776794
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-06-01 06:56:25 +0700

    text area implementation(29)

--- src/components/commentBox.js
+++ src/components/commentBox.js
@@ -1,14 +1,29 @@
 import React from 'react'
 
-const CommentBox = () => {
+class CommentBox extends React.Component {
+	state = {
+		comment: ''
+	}
+	handleChange = (e) => {
+		this.setState({
+			comment: e.target.value
+		})
+	}
+	handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(this.state.comment)
+	}
+	render() {
+		let { comment } = this.state
     return (
-			<form>
+			<form onSubmit={this.handleSubmit}>
 				<h4>Add a comment</h4>
-				<textarea />
+				<textarea onChange={this.handleChange} value={comment}/>
 				<div>
 					<button>submit comment</button>
 				</div>
 			</form>
 		)
+	}
 }
 export default CommentBox
