9eea9c1f63571dd8f671c09661e81e50b06c39eb
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-06-01 07:15:32 +0700

    afterEach statement

--- src/__test__/componentBox.test.js
+++ src/__test__/componentBox.test.js
@@ -2,8 +2,18 @@ import React from 'react'
 import CommentBox from 'components/commentBox'
 import { mount } from 'enzyme';
 
+let wrapped
+
+beforeEach(()=> {
+	wrapped = mount(<CommentBox />);
+})
+
+afterEach(()=> { // function helper dari jest yang akan jalan setelah semua test selesai
+	wrapped.unmount()
+})
+
 it('has a text area and button', ()=> {
-	const wrapped = mount(<CommentBox />);
+
 	console.log(wrapped.find('textarea').length)
 	console.log(wrapped.find('button').length)
 
