2e3b69572501a566efe623fb3f454c1f4e72e246
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-06-01 07:54:14 +0700

    retrieving prop value(37)

--- src/__test__/componentBox.test.js
+++ src/__test__/componentBox.test.js
@@ -18,7 +18,11 @@ it('has a text area and button', ()=> {
 })
 
 it('user can type in text area', ()=> {
+	// const textArea = wrapped.find('textarea') // jangan dipisah gni bakal error, mungkin karna pass value by reference ?
 	wrapped.find('textarea').simulate('change', {
 		target: { value: 'new comment' } // target dan value mengikuti dari balikan event callback onChange
 	}) //menggunakan html event bkn react event(onChange)
+
+	wrapped.update() // engga pake ini juga tetep pass sebenernya, gunanya ini untuk nge force update dom karna ada setState pas merubah value yang bakal nge re render component secara async tidak instant. makanya perlu ini agar instant nge re render nya
+	expect(wrapped.find('textarea').prop('value')).toEqual('new comment') // prop yang ada di tag textarea (bisa prop apa aja custom prop juga bisa)
 })
