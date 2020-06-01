5471767735a78a7ad4026c09d6613a149ded36f4
Mahaldi Wijaya Saputra <aldi@umroh.com>
2020-06-01 14:05:34 +0700

    describe statement(40)

--- src/__test__/componentBox.test.js
+++ src/__test__/componentBox.test.js
@@ -17,24 +17,24 @@ it('has a text area and button', ()=> {
 	expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('user can type in text area', ()=> {
-	// const textArea = wrapped.find('textarea') // jangan dipisah gni bakal error, mungkin karna pass value by reference ?
-	wrapped.find('textarea').simulate('change', {
-		target: { value: 'new comment' } // target dan value mengikuti dari balikan event callback onChange
-	}) //menggunakan html event bkn react event(onChange)
-
-	wrapped.update() // engga pake ini juga tetep pass sebenernya, gunanya ini untuk nge force update dom karna ada setState pas merubah value yang bakal nge re render component secara async tidak instant. makanya perlu ini agar instant nge re render nya
-	expect(wrapped.find('textarea').prop('value')).toEqual('new comment') // prop yang ada di tag textarea (bisa prop apa aja custom prop juga bisa)
-})
-
-it('when form submitted, textarea get empty', ()=> {
-	wrapped.find('textarea').simulate('change', {
-		target: {value: 'new comment'}
+describe('the text area', ()=> { // berguna untuk mengelompokan test, dan akan berbeda dengan yang diluar, ini karna ingin menggunakan before each lagi. tapi tidak ingin mengubah beforeeach yang sudah ada
+	beforeEach(()=> {
+		// const textArea = wrapped.find('textarea') // jangan dipisah gni bakal error, mungkin karna pass value by reference ?
+		wrapped.find('textarea').simulate('change', {
+			target: { value: 'new comment' } // target dan value mengikuti dari balikan event callback onChange
+		}) //menggunakan html event bkn react event(onChange)
+		wrapped.update() // engga pake ini juga tetep pass sebenernya, gunanya ini untuk nge force update dom karna ada setState pas merubah value yang bakal nge re render component secara async tidak instant. makanya perlu ini agar instant nge re render nya
 	})
-	wrapped.update()
+	it('user can type in text area', ()=> {
+		expect(wrapped.find('textarea').prop('value')).toEqual('new comment') // prop yang ada di tag textarea (bisa prop apa aja custom prop juga bisa)
+	})
 
-	wrapped.find('form').simulate('submit')
-	wrapped.update()
+	it('when form submitted, textarea get empty', ()=> {
 
-	expect(wrapped.find('textarea').prop('value')).toEqual('')
+		wrapped.find('form').simulate('submit')
+		wrapped.update()
+
+		expect(wrapped.find('textarea').prop('value')).toEqual('')
+	})
+
 })
